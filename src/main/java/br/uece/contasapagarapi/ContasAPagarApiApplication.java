d176e7bca13d84d3ec1585c185a7eb4fc9fcb3c5
eduardo9200 <edumaia92@gmail.com>
2024-04-04 09:49:51 -0300

    Criação do esqueleto do projeto

---
+++ src/main/java/br/uece/contasapagarapi/ContasAPagarApiApplication.java
@@ -0,0 +1,13 @@
+package br.uece.contasapagarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasAPagarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasAPagarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uece/contasapagarapi/ContasAPagarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.contasapagarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasAPagarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
